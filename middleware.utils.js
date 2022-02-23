d38acc8825fd86f9e2ea6c70015e71c1053452e2
Prateek Goyal <gprateek015@gmail.com>
2022-02-23 12:04:08 +0530

    updated middleware

--- middleware.utils.js
+++ middleware.utils.js
@@ -1,7 +1,7 @@
 module.exports.authMiddleware = (req, res, next) => {
-  console.log(req.headers['x-api-authentication']);
   if (req.headers['x-api-authentication'] === process.env.ADMIN_UID) {
     next();
+  } else {
+    res.status(501).send('Not authorised attempt');
   }
-  res.status(501).send('Not authorised attempt');
 };
