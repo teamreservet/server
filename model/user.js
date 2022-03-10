88e5335de4f2c58c19f30b1fb1a803708e5c689f
Prateek Goyal <gprateek015@gmail.com>
2022-03-10 09:59:50 +0530

    updated user model

--- model/user.js
+++ model/user.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  uid: {
-    type: String,
-    required: true
-  },
   displayName: {
     type: String,
     required: true
